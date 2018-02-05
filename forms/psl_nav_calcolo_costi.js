c87fc007acb46e732c87186b1b597a149d3cb7b2
Giovanni Tizzoni <giovanni.tizzoni@gmail.com>
2018-02-05 17:42:47 +0100

    First commit

---
+++ forms/psl_nav_calcolo_costi.js
@@ -0,0 +1,22 @@
+/**
+ * @properties={typeid:24,uuid:"71755713-32CC-4650-94A2-62C2094A12D6"}
+ */
+function getName()
+{
+	return scopes.psl.Sezioni.CALCOLOCOSTI['nome'];
+}
+/**
+ * Callback method for when form is shown.
+ *
+ * @param {Boolean} firstShow form is shown first time after load
+ * @param {JSEvent} event the event that triggered the action
+ *
+ * @private
+ *
+ * @properties={typeid:24,uuid:"EB9DF3D6-7F52-465D-A259-66B529A1B49A"}
+ */
+function onShow(firstShow, event)
+{
+	if(firstShow)
+		plugins.busy.prepare();
+}
